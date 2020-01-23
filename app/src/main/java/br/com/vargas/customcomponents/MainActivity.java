a3ef67fdcbd4a9cb504dcc6ba5ee98a356a10b3b
Gabriel Vargas <assuncao.gabriel12@gmail.com>
2020-01-22 22:32:42 -0300

    Cria componente usando inflate layout

--- app/src/main/java/br/com/vargas/customcomponents/MainActivity.java
+++ app/src/main/java/br/com/vargas/customcomponents/MainActivity.java
@@ -3,12 +3,57 @@ package br.com.vargas.customcomponents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import br.com.vargas.customcomponents.components.TextWithIconTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextWithIconTextView text1 = findViewById(R.id.text_1);
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "click text1");
+                changeStatus(text1);
+            }
+        });
+
+        final TextWithIconTextView text2 = findViewById(R.id.text_2);
+        text2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "click text1");
+                changeStatus(text2);
+            }
+        });
+    }
+
+    private void changeStatus(TextWithIconTextView text) {
+        int atualStatus = text.getStatus();
+
+        switch (atualStatus) {
+            case TextWithIconTextView.SUCCESS:
+                text.setStatus(TextWithIconTextView.ERROR);
+                break;
+
+            case TextWithIconTextView.ERROR:
+                text.setStatus(TextWithIconTextView.RUNNING);
+                break;
+
+            case TextWithIconTextView.RUNNING:
+                text.setStatus(TextWithIconTextView.WAITING);
+                break;
+
+            default:
+                text.setStatus(TextWithIconTextView.SUCCESS);
+        }
     }
 }
---
+++ app/src/main/java/br/com/vargas/customcomponents/components/TextWithIconTextView.java
@@ -0,0 +1,101 @@
+package br.com.vargas.customcomponents.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+import br.com.vargas.customcomponents.R;
+
+public class TextWithIconTextView extends LinearLayout  {
+
+    private ImageView icon;
+    private Drawable iconError;
+    private Drawable iconRunning;
+    private Drawable iconSuccess;
+    private Drawable iconWaiting;
+
+    private int status;
+    public static final int WAITING = 0;
+    public static final int RUNNING = 1;
+    public static final int SUCCESS = 2;
+    public static final int ERROR = 3;
+
+    public TextWithIconTextView(Context context) {
+        super(context);
+    }
+
+    public TextWithIconTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public TextWithIconTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TextWithIconTextView, 0, 0);
+        String text = a.getString(R.styleable.TextWithIconTextView_text);
+        iconError = a.getDrawable(R.styleable.TextWithIconTextView_iconError);
+        iconRunning = a.getDrawable(R.styleable.TextWithIconTextView_iconRunning);
+        iconSuccess = a.getDrawable(R.styleable.TextWithIconTextView_iconSuccess);
+        iconWaiting = a.getDrawable(R.styleable.TextWithIconTextView_iconWaiting);
+
+        status = a.getInt(R.styleable.TextWithIconTextView_status, 0);
+
+        a.recycle();
+
+
+        setOrientation(LinearLayout.HORIZONTAL);
+        setGravity(Gravity.CENTER_VERTICAL);
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.text_with_icon_component, this, true);
+
+        TextView title = (TextView) getChildAt(0);
+        title.setText(text);
+
+        icon = (ImageView) getChildAt(1);
+        setIconStatus();
+
+    }
+
+    private void setIconStatus() {
+        switch (status) {
+            case SUCCESS:
+                icon.setImageDrawable(iconSuccess);
+                break;
+
+            case ERROR:
+                icon.setImageDrawable(iconError);
+                break;
+
+            case RUNNING:
+                icon.setImageDrawable(iconRunning);
+                break;
+
+            default:
+                icon.setImageDrawable(iconWaiting);
+        }
+    }
+
+    public void setStatus(int newStatus) {
+        this.status = newStatus;
+        setIconStatus();
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
+}
